3203f6f762f6ccce7d59e009109ece6c12f4b103
Sean Spradlin <sts@smartvue.com>
2016-03-29 13:39:55 -0500

    First commit

---
+++ TankHero/Game1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Input;
+
+namespace TankHero
+{
+	public class TankHero : Game
+	{
+		GraphicsDeviceManager graphics;
+		SpriteBatch spriteBatch;
+
+		public TankHero ()
+		{
+			graphics = new GraphicsDeviceManager (this);
+			Content.RootDirectory = "Content";
+		}
+
+		protected override void LoadContent ()
+		{
+			spriteBatch = new SpriteBatch (GraphicsDevice);
+		}
+
+		protected override void Update (GameTime gameTime)
+		{
+			#if !__IOS__ &&  !__TVOS__
+			if (GamePad.GetState (PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState ().IsKeyDown (Keys.Escape))
+				Exit ();
+			#endif
+			base.Update (gameTime);
+		}
+
+		protected override void Draw (GameTime gameTime)
+		{
+			graphics.GraphicsDevice.Clear (Color.CornflowerBlue);
+			base.Draw (gameTime);
+		}
+	}
+}
+
