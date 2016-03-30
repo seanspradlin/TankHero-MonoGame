1e9a1af051d039358c5f4189e420c30df7bdaef6
Sean Spradlin <sts@smartvue.com>
2016-03-30 08:46:59 -0500

    Unix line endings, again...

--- TankHero/Engine/Animation.cs
+++ TankHero/Engine/Animation.cs
@@ -4,38 +4,35 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace TankHero.Engine
 {
-    public class Animation
-    {
-        int currentFrame = 0;
-        Texture2D[] frames;
-        AnimationManager manager;
+	public class Animation
+	{
+		int currentFrame = 0;
+		Texture2D[] frames;
+		AnimationManager manager;
 
-        public Animation(AnimationManager m, Texture2D[] f, int fps = 0)
-        {
-            if (f.Length == 0)
-            {
-                throw new Exception("There must be at least one texture");
-            }
+		public Animation (AnimationManager m, Texture2D[] f, int fps = 0)
+		{
+			if (f.Length == 0) {
+				throw new Exception ("There must be at least one texture");
+			}
 
-            manager = m;
-            Game = m.Game;
-            frames = f;
-            FramesPerSecond = fps;
-        }
+			manager = m;
+			Game = m.Game;
+			frames = f;
+			FramesPerSecond = fps;
+		}
 
-        public Texture2D Frame
-        {
-            get
-            {
-                return frames[currentFrame];
-            }
-        }
+		public Texture2D Frame {
+			get {
+				return frames [currentFrame];
+			}
+		}
 
-        public int FramesPerSecond { get; set; }
+		public int FramesPerSecond { get; set; }
 
-        public string Key { get; }
+		public string Key { get; }
 
-        public IGame Game { get; }
-    }
+		public IGame Game { get; }
+	}
 }
 
